96babef15a8bac2e94e124acdbb0b43a031d9d6c
yago1994 <yagoaral@hotmail.com>
2022-05-13 15:46:46 -0400

    firebase setup;

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,33 @@
+// Firebase 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC0oHcnRMNo_92xxYWwLwmKIi9tp89Yz6g",
+    authDomain: "herheart-5ca67.firebaseapp.com",
+    projectId: "herheart-5ca67",
+    storageBucket: "herheart-5ca67.appspot.com",
+    messagingSenderId: "435629529947",
+    appId: "1:435629529947:web:77da4219b313ff7b5b987e",
+    measurementId: "G-6Z1Z3BMDQ4"
+};
+  
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+  
+  
+function writeUserData(answers) {
+    const db = getDatabase();
+    // Get a key for a new Post.
+    const entryKey = push(ref(db)).key;
+    const updates = {};
+    updates['users/' + entryKey] = answers;
+    update(ref(db), updates);
+}
+
 Survey
     .Serializer
     .addProperty("page", "animated:text");
