c3f3ce989a806c5e72cbc7c3e112b162a6ac7b59
Comfort Mwalija <calmwalija@gmail.com>
2022-05-27 13:52:02 +0200

    ** Minor UI tweaks

--- public/js/json.js
+++ public/js/json.js
@@ -721,7 +721,7 @@ const json = {
                 {
                     "type": "html",
                     "name": "test",
-                    "html": "<div id='sketch-holder'>" +
+                    "html": "<div id='sketch-holder'></div>" +
                         "<h3><center> Oh! You are 21+, let me ask you then, do you drink alcohol??</center></h3>" +
                         "</div><img alt='' style='margin-left:auto; margin-right:auto; display:block; width:50%;' src='img/alcohol.png'>" +
                         "<p><center>One glass = 4 ounces of wine, 12 ounces of beer</center></p>"
